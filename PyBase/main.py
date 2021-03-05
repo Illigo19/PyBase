9d2ef97299f25f94c9ad840d4e1f44a672487cf0
Illigo19 <67434468+Illigo19@users.noreply.github.com>
2021-03-05 19:22:38 +0100

    Update main.py

--- PyBase/main.py
+++ PyBase/main.py
@@ -10,20 +10,20 @@ data = obj.getData("myfile")
 
 
 # create a New Table, check if her already exist when newTab(data,table,1 or None) not when newTab(data,table,0)
-#obj.newTab(data, "Louis")
+obj.newTab(data, "Louis")
 
 # check if a Table (here : Hello world) exist in data (bool)
-#if obj.findTab(data, "Hello world"):
+if obj.findTab(data, "Hello world"):
 	#print("exist")
 
 #delete table with her name (return True if it delete a Table) WARNING can delete the complet data, tape the exact name 
-#obj.delTab(data, "Name")
+obj.delTab(data, "Name")
 
 #add content into a table 
 obj.appendTab(data, "Louis", "Joseph")
 
 # delete from Table a content (here : content)
-#obj.delCont(data, "Louis", "Joseph")
+obj.delCont(data, "Louis", "Joseph")
 
 
 obj.getCont(data, "Louis")
